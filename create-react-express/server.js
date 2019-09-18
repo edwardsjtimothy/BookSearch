e3fdbd8160944a59dc6cd9bc701e974023a0cab5
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-18 11:31:43 -0500

    get request working

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const axios = require("axios");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,10 +13,37 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
 // Define API routes here
+let bookSearch = "HarryPotter";
+let queryUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`;
 
-
-
+axios.get(queryUrl).then(
+  function(response) {
+    let data = response.kind
+    console.log(data);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
 // Send every other request to the React app
 
 
