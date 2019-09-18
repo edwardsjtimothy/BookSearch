721dc9aca9ab67c999e5577a0fcb01621a370ab1
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-18 10:01:17 -0500

    deleted extras

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -13,7 +13,12 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
+
+
 // Send every other request to the React app
+
+
+
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
