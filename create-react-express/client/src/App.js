27253acee92641b16666296486f545326a64e252
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-19 15:37:25 -0500

    added jumbotron

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello!</h1>
+        <Jumbotron />
       </div>
     );
   }
---
+++ create-react-express/client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function jumbotron() {
+    return (
+        <>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Book Search</h1>
+                    <p class="lead">Search for you favorite books and store them in a database!</p>
+                </div>
+            </div>
+        </>
+    )
+}
