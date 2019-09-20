85ed319698e162bb41404b36be6c5e5d1f82ae45
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-19 21:10:50 -0500

    state setting correctly

--- create-react-express/client/src/components/Body/index.js
+++ create-react-express/client/src/components/Body/index.js
@@ -1,7 +1,61 @@
 import React, { Component } from 'react'
 import "./body.css";
+import axios from "axios";
+
 
 export default class Body extends Component {
+    
+    state = {
+        title: "",
+        booksArray: []
+    };
+    
+    bookSearch =(event)=> {
+        let self=this
+        event.preventDefault();
+        let bookSearch = document.getElementById("search-input").value;
+        console.log(bookSearch);
+        let queryUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`;
+        let bookArray =this.state.booksArray;
+        axios.get(queryUrl).then(
+            function(response) {
+                for (let i = 0; i <= 9; i++) {
+                    let data = response.data.items[i].volumeInfo;
+                    bookArray.push(data);
+                }
+                self.setState({bookArray});
+    })
+    .catch(function(error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error);
+        }
+        console.log(error.config);
+    });
+    
+}
+
+
+    handleChange =(event)=> {
+        let oldState = this.state;
+        oldState.title = event.target.value;
+        this.setState(oldState);
+    }
+    
+    
     render() {
         return (
             <div className="container">
@@ -13,10 +67,10 @@ export default class Body extends Component {
                                 <div className="form-row align-items-center">
                                     <div className="col-auto">
                                         <label className="sr-only" for="inlineFormInput">Book</label>
-                                        <input type="text" className="form-control mb-2 search-input" placeholder="American Gods by Neil Gaiman" />
+                                        <input id="search-input" type="text" className="form-control mb-2 search-input" value= {this.state.title} onChange={this.handleChange} placeholder="American Gods by Neil Gaiman" />
                                     </div>
                                     <div className="col-auto">
-                                        <button type="submit" className="btn btn-success mb-2">Search</button>
+                                        <button type="submit" className="btn btn-success mb-2" onClick={this.bookSearch}>Search</button>
                                     </div>
                                 </div>
                             </form>
--- create-react-express/server.js
+++ create-react-express/server.js
@@ -16,38 +16,6 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
 // Define API routes here
-let bookSearch = "HarryPotter";
-let queryUrl = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`;
-
-axios.get(queryUrl).then(
-  function(response) {
-    let data = response.data.items;
-    let title = data[0].volumeInfo.title;
-    let author = data[0].volumeInfo.authors;
-    let synopsis = data[0].volumeInfo.description;
-    let image = data[0].volumeInfo.imageLinks.smallThumbnail;
-    let info = data[0].volumeInfo.infoLink;
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
 // Send every other request to the React app
 
 
